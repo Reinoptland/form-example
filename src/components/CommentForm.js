81ab6b41aea3e37f152c826c821a88015e5bc07c
Reinoptland <reinoptland@gmail.com>
2021-01-08 13:56:40 +0100

    Registreer invoervelden en handle submit

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,23 +1,29 @@
 import "./CommentForm.css";
+import { useForm } from "react-hook-form";
 
 export default function CommentForm() {
+  const { register, handleSubmit } = useForm();
+  //   console.log("WAT KOMT ER UIT USEFORM?", what);
   // name: input -> type text & label
   // email: input -> type email & label
   // body: textarea -> type text & label
   // postId: select -> options & label
+  function postComment(data) {
+    console.log("WAT IS ER INGEVULD:", data);
+  }
 
   return (
     <div>
       <h2>COMMENT FORM</h2>
-      <form>
+      <form onSubmit={handleSubmit(postComment)}>
         <label htmlFor="voornaam">Naam</label>
-        <input name="voornaam" type="text" />
+        <input name="voornaam" type="text" ref={register} />
         <label htmlFor="email">Email</label>
-        <input name="email" type="email" />
+        <input name="email" type="email" ref={register} />
         <label htmlFor="comment">Comment</label>
-        <textarea name="comment" cols="30" rows="10"></textarea>
+        <textarea name="comment" cols="30" rows="10" ref={register}></textarea>
         <label htmlFor="postId">Op welke post wil je reageren</label>
-        <select name="postId">
+        <select name="postId" ref={register}>
           <option>1</option>
           <option>2</option>
           <option>3</option>
